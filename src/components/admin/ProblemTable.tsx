e0922706a9cc17cdb227eaf94b2ece7483d4a1d0
nhubaole <okamisameko7123@gmail.com>
2024-12-21 23:53:53 +0700

    ui admin

--- src/components/admin/ProblemTable.tsx
+++ src/components/admin/ProblemTable.tsx
@@ -1,4 +1,3 @@
-
 type Problem = {
   title: string;
   difficulty: string;
@@ -9,53 +8,54 @@ type Problem = {
 
 const problems: Problem[] = [
   {
-    title: 'Two Sum',
-    difficulty: 'Easy',
-    explaination: 'Design',
-    tag: 'Hash Map, Array',
-    created_at: '12/10/2021',
+    title: "Two Sum",
+    difficulty: "Easy",
+    explaination: "Design",
+    tag: "Hash Map, Array",
+    created_at: "12/10/2021",
   },
   {
-    title: 'Palindrome Number',
-    difficulty: 'Easy',
-    explaination: 'Design',
-    tag: 'Hash Map, Array',
-    created_at: '12/10/2021',
+    title: "Palindrome Number",
+    difficulty: "Easy",
+    explaination: "Design",
+    tag: "Hash Map, Array",
+    created_at: "12/10/2021",
   },
   // Add more employee data here...
 ];
 
 const ProblemTable = () => {
-    return (
-        <div className="p-4">
-          <table className="w-full text-left border-collapse ">
-            <thead className="bg-gray-800">
-              <tr>
-                <th className="p-3 border-b border-[#A2A1A833]">Title</th>
-                <th className="p-3 border-b border-[#A2A1A833]">Difficulty</th>
-                <th className="p-3 border-b border-[#A2A1A833]">Explanation</th>
-                <th className="p-3 border-b border-[#A2A1A833]">Tag</th>
-                <th className="p-3 border-b border-[#A2A1A833]">Created Time</th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-[#A2A1A833]">
-              {problems.map((problem, index) => (
-                <tr
-                  key={index}
-                  className="hover:bg-gray-800 transition-colors duration-200"
-                >
-                  <td className="p-5">{problem.title}</td>
-                  <td className="p-5">{problem.difficulty}</td>
-                  <td className="p-5">{problem.explaination}</td>
-                  <td className="p-5">{problem.tag}</td>
-                  <td className="p-5">{problem.created_at}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
-      
+  return (
+    <div className="p-4">
+      <table className="w-full text-left border-collapse ">
+        <thead className="bg-gray-800">
+          <tr>
+            <th className="p-3 border-b border-[#A2A1A833]">Title</th>
+            <th className="p-3 border-b border-[#A2A1A833]">Difficulty</th>
+            <th className="p-3 border-b border-[#A2A1A833]">Explanation</th>
+            <th className="p-3 border-b border-[#A2A1A833]">Tag</th>
+            <th className="p-3 border-b border-[#A2A1A833]">Created Time</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-[#A2A1A833]">
+          {problems.map((problem, index) => (
+            <tr
+              key={index}
+              className="hover:bg-gray-800 transition-colors duration-200"
+            >
+              <td className="p-5">{problem.title}</td>
+              <td className="p-5 m-5 ">
+                <span className="bg-[#7152F31A] text-green-500 px-3 py-1 rounded-md" >{problem.difficulty}</span>
+              </td>
+              <td className="p-5">{problem.explaination}</td>
+              <td className="p-5">{problem.tag}</td>
+              <td className="p-5">{problem.created_at}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ProblemTable;
